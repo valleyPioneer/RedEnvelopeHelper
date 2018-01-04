7a96ef8377c55be7b0b44f5d47758c57de0a27bf
valleyPioneer <xiaoxuzhang@ebupt.com>
2018-01-04 22:27:57 +0800

    try but not fixed!

---
+++ app/src/main/java/com/example/valleypioneer/redenvelopehelper/Constants.java
@@ -0,0 +1,19 @@
+package com.example.valleypioneer.redenvelopehelper;
+
+/**
+ * Created by valleypioneer on 2018/1/4.
+ */
+
+public class Constants {
+    /** 聊天界面ID */
+    public static final String GET_RED_ENVELOPE = "com.tencent.mm:id/ada";
+    public static final String LAUNCHER_UI_BACK = "com.tencent.mm:id/h9";
+    /** 抢红包界面ID */
+    public static final String GRAB_PARENT = "com.tencent.mm:id/c04";
+    public static final String CANCEL = "com.tencent.mm:id/c07";
+    public static final String GRAB = "com.tencent.mm:id/c2i";
+    /** 红包详情界面ID */
+    public static final String LUCKY_MONEY_DETAIL_UI_BACK = "com.tencent.mm:id/ho";
+    public static final String LEAVE_MESSAGE = "com.tencent.mm:id/bzd";
+
+}
--- app/src/main/java/com/example/valleypioneer/redenvelopehelper/RedEnvelopeService.java
+++ app/src/main/java/com/example/valleypioneer/redenvelopehelper/RedEnvelopeService.java
@@ -5,7 +5,9 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
@@ -13,11 +15,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RedEnvelopeService extends AccessibilityService {
-    private List<AccessibilityNodeInfo> parents;
+    private static final String TAG = "RedEnvelopeService";
+    private boolean isProcessing = false; /** 记录是否处于一轮抢红包的状态中 */
+
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
-        parents = new ArrayList<>();
     }
 
     @Override
@@ -25,54 +28,102 @@ public class RedEnvelopeService extends AccessibilityService {
         int eventType = event.getEventType();
         switch (eventType){
             case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
-                List<CharSequence> texts = event.getText();
-                if(!texts.isEmpty()){
-                    for(CharSequence text : texts){
-                        String content = text.toString();
-                        if(content.contains("[微信红包 ]")){
-                            if(event.getParcelableData() != null && event.getParcelableData() instanceof Notification){
-                                Notification notification = (Notification)event.getParcelableData();
-                                PendingIntent pi = notification.contentIntent;
-                                try{
-                                    pi.send();
-                                }catch (Exception e){
-                                    e.printStackTrace();
+                if(!isProcessing){
+                    List<CharSequence> texts = event.getText();
+                    Log.i(TAG,"notification texts is " + texts.toString());
+                    if(!texts.isEmpty()){
+                        for(CharSequence text : texts){
+                            String content = text.toString();
+                            if(content.contains("[微信红包]")){
+                                if(event.getParcelableData() != null && event.getParcelableData() instanceof Notification){
+                                    isProcessing = true; /** 开始处理本次通知 */
+                                    Notification notification = (Notification)event.getParcelableData();
+                                    PendingIntent pi = notification.contentIntent;
+                                    try{
+                                        pi.send();
+                                    }catch (Exception e){
+                                        e.printStackTrace();
+                                    }
                                 }
                             }
                         }
                     }
                 }
+
                 break;
                 case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
                     String className = event.getClassName().toString();
+                    Log.i(TAG,"className is " + className);
+
                     if (className.equals("com.tencent.mm.ui.LauncherUI")) {
-                        //点击最后一个红包
-                        getLastPacket();
+                        if(isProcessing){
+                            Log.i(TAG,"开始领取红包！");
+                            getLastPacket(Constants.GET_RED_ENVELOPE);
+                        }
+
                     } else if (className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI")) {
-                        //开红包
-                        inputClick("com.tencent.mm:id/bg7");
+                        Log.i(TAG,"开始抢红包！");
+                        checkMoneyLeft(Constants.GRAB);
                     } else if (className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI")) {
-                        //退出红包
-                        inputClick("com.tencent.mm:id/gd");
+                        if(isProcessing){
+                            Log.i(TAG,"退出红包详情界面！");
+                            /** 退出红包详情界面，返回聊天界面 */
+                            performGlobalAction(GLOBAL_ACTION_BACK);
+                            performGlobalAction(GLOBAL_ACTION_HOME);
+                        }
+                        isProcessing = false; /** 完成此次通知处理,准备下次通知处理*/
                     }
                     break;
+                default:
         }
     }
 
-    private void inputClick(String widgetID){
+    private void checkMoneyLeft(String widgetID){
         AccessibilityNodeInfo root = getRootInActiveWindow();
+        for(int i = 0;i < root.getChildCount();i++){
+            Log.i(TAG,"child is " + root.getChild(i));
+        }
+        Log.i(TAG,"rootNode2 id is" + root.getClassName());
         if(root != null){
             List<AccessibilityNodeInfo> nodeslist = root.findAccessibilityNodeInfosByViewId(widgetID);
-            for(AccessibilityNodeInfo node : nodeslist)
-                node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            Log.i(TAG,"nodesList.size() is" + nodeslist.size());
+            if(nodeslist.size() != 0) {
+                for(AccessibilityNodeInfo node : nodeslist)
+                    node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                return;
+            }
         }
+        isProcessing = false;
+        performGlobalAction(GLOBAL_ACTION_BACK);
     }
 
-    private void getLastPacket(){
-        //文字匹配法？控件id法？
+    private boolean inputNormalClick(String widgetID){
         AccessibilityNodeInfo root = getRootInActiveWindow();
+        boolean res = true;
         if(root != null){
-           // List<AccessibilityService> nodesList = root.findAccessibilityNodeInfosByText()
+            List<AccessibilityNodeInfo> nodeslist = root.findAccessibilityNodeInfosByViewId(widgetID);
+            Log.i(TAG,"nodesList.size() is" + nodeslist.size());
+            if (nodeslist.size() == 0)
+                res = false;
+            else
+                for(AccessibilityNodeInfo node : nodeslist)
+                    node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        }
+        return res;
+    }
+
+    private void getLastPacket(String widgetID){
+        /** 建议使用控件id法效率更高 */
+        AccessibilityNodeInfo root = getRootInActiveWindow();
+        for(int i = 0;i < root.getChildCount();i++){
+            Log.i(TAG,"1.....child is " + root.getChild(i));
+        }
+        Log.i(TAG,"rootNode1 id is" + root.getClassName());
+        if(root != null){
+            List<AccessibilityNodeInfo> nodesList = root.findAccessibilityNodeInfosByViewId(widgetID);
+            Log.i(TAG,"getpacket is size" + nodesList.size());
+            /** 对最后一个"领取红包"进行点击 */
+            nodesList.get(nodesList.size() - 1).performAction(AccessibilityNodeInfo.ACTION_CLICK);
         }
     }
 
@@ -81,4 +132,6 @@ public class RedEnvelopeService extends AccessibilityService {
     public void onInterrupt() {
 
     }
+
+
 }
